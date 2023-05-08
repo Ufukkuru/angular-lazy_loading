a97bbc2251a84ac4ef2eff142cb00e238ac8587b
ufuk kuru <uege60@gmail.com>
2023-05-08 17:59:00 +0300

    Angular Lazy_loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { LoginComponent } from './component/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {
+        path:"login",
+        loadChildren:()=> import("./component/login/login.module").then(m => m.LoginModule),
+        component:LoginComponent
+      },
+      {
+        path:"",
+        loadChildren:()=> import("./component/layouts/layouts.module").then(m => m.LayoutsModule),
+      },
+      {
+        path:"**",
+        loadChildren:() => import("./component/not-found/not-found.module").then(m => m.NotFoundModule),
+        component:NotFoundComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
